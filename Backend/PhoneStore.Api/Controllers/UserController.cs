b41fca55ac1c7501430834681e7d9d71cc58596c
thehieu03 <153330033+thehieu03@users.noreply.github.com>
2025-03-31 14:03:42 +0700

    forget password

--- Backend/PhoneStore.Api/Controllers/UserController.cs
+++ Backend/PhoneStore.Api/Controllers/UserController.cs
@@ -206,14 +206,18 @@ namespace PhoneStore.Api.Controllers
             if (account == null)
                 return NotFound(new { success = false, message = "Không tìm thấy tài khoản." });
 
-            account.PassWord = request.NewPassword; // Bạn nên hash password ở đây
+            account.PassWord = HasBCrypt(request.NewPassword); // Bạn nên hash password ở đây
             var result = await _accountServices.UpdateAsync(account);
             if (result)
                 return Ok(new { success = true, message = "Đặt lại mật khẩu thành công." });
 
             return StatusCode(500, new { success = false, message = "Lỗi hệ thống khi đặt lại mật khẩu." });
         }
-
+        [NonAction]
+        private static string HasBCrypt(string pass)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(pass);
+        }
         public class ResetPasswordRequest
         {
             public string Email { get; set; } = string.Empty;
